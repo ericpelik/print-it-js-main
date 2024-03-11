d58c0e8c9cbc7080642f8872fd081e5f90ebc6f9
ericpelik <pleyber.eric@gmail.com>
2024-03-11 19:12:59 +0100

    mise à jour du code HTML + Création des Bullet Points + Mise en place des eventListeners et du defilement infini du carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,52 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/** Création de defilement infini du carrousel */
+
+const bannerImage = document.querySelector(".banner-img");
+
+let tagLine = document.querySelector("#banner p")
+
+let compteur=0;
+
+/** ajout des eventListeners */
+/** Flèche droite  - Avec modifications au clic */
+
+let next=document.querySelector(".arrow_right")
+console.log(next);
+
+next.addEventListener("click", () => {
+    compteur++
+    if (compteur>=slides.length){
+        compteur=0
+    }  
+    bannerImage.src="./assets/images/slideshow/"+slides[compteur].image;
+    tagLine.innerHTML = slides[compteur].tagLine;
+    console.log(compteur)
+})
+
+/** Flèche gauche  - Avec modifications au clic */
+
+let prev=document.querySelector(".arrow_left")
+console.log(next);
+
+prev.addEventListener("click", () => {
+    compteur--
+    if (compteur<0){
+        compteur=slides.length-1
+    }  
+    bannerImage.src="./assets/images/slideshow/"+slides[compteur].image;
+    tagLine.innerHTML = slides[compteur].tagLine;
+    console.log(compteur)
+})
+
+/** Création des bullet points */
+
+const dots = document.querySelector(".dots")
+for (let i=0; i<slides.length; i++){
+    let dot=document.createElement("div");
+    dot.className="dot";
+    dots.appendChild(dot)
+	console.log(dots)
+}
